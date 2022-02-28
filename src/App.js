8131864cc3ba26255767ca19d3fbe80cfade94d4
kukkusuresh <kukkusuresh18@gmail.com>
2022-02-28 14:02:58 +0530

    second

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import { Component } from 'react';
+import './App.css';
+import Login from "./component/login/Login";
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Login/>
+    </div>
+   
+  );
+}
+
+export default App;
